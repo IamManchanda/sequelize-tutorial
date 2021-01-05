3a1ae6d395b9bdf162c35fd4bd2e71cd8668bf89
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-06 01:48:36 +0530

    Create user endpoint

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const { sequelize } = require("./models");
+const express = require("express");
+const { sequelize, User } = require("./models");
 
-(async function initializeConnection() {
+const app = express();
+app.use(express.json());
+
+app.post("/users", async function createUser(req, res) {
+  const { name, email, role } = req.body;
+  try {
+    const user = await User.create({ name, email, role });
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Something went wrong, Internal Server Error.",
+    });
+  }
+});
+
+app.listen({ port: 5000 }, async function bootApp() {
+  console.log("Server listening on http://localhost:5000");
+  console.log("Database sync started");
   await sequelize.sync({ force: true });
-})();
+  console.log("Database sync completed");
+});
